1d276918fa75ef50dade035734cd6e27273834fb
stewitt <stefan.wittlinger@stud.uni-heidelberg.de>
2025-06-21 16:34:20 +0200

    Cleaned up code

--- classification_readmission.py
+++ classification_readmission.py
@@ -1,12 +1,8 @@
 #===Imports===
-#import sys
 import pickle
 import numpy as np
 import pandas as pd
-#import seaborn as sns
-#import torch
 import shap
-#import matplotlib.pyplot as plt
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
